b4e8eb7b1e460fb7b8ddd41022b43ad35a8f4baa
Matan Weiss <matan1111@gmail.com>
2023-05-20 14:42:58 +0000

    findAliveMember checks distance according to the attacking teams' capitan

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -88,8 +88,8 @@ Character *Team::findAliveMember(Team *team) {
   double shortest = DBL_MAX;
   Character *result = NULL;
   for (Character *member : team->members)
-    if (member->isAlive() && team->leader->distance(member) < shortest) {
-      shortest = team->leader->distance(member);
+    if (member->isAlive() && leader->distance(member) < shortest) {
+      shortest = leader->distance(member);
       result = member;
     }
   return result;
